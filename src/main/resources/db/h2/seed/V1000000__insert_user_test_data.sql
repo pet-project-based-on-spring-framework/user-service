522f4da09ef9a3e802b7743dc0d53e506d267697
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2021-01-29 20:38:47 +0100

    feat(migration) adding user migration schema and test data (master)

---
+++ src/main/resources/db/h2/seed/V1000000__insert_user_test_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO usr (id, first_name, last_name, username, email, password, birthday, created_date, updated_date)
+VALUES (1, 'Roman', 'Tsyupryk', 'TRL', 'tsyupryk.roman@gmail.com', 'strong_password', '1988-06-26', now(), now()),
+       (2, 'first_name_test_1', 'last_name_test_1', 'username_test_1', 'test_email_1@mail.com', 'strong_password', '2000-01-01', now(), now()),
+       (3, 'first_name_test_2', 'last_name_test_2', 'username_test_2', 'test_email_2@mail.com', 'strong_password', '2000-01-02', now(), now()),
+       (4, 'first_name_test_3', 'last_name_test_3', 'username_test_3', 'test_email_3@mail.com', 'strong_password', '2000-01-03', now(), now());
