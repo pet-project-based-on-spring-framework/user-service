65f57199959acbf834ab12c5f7d46420b2598d4f
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2021-01-29 13:51:12 +0100

    feat() first comit (master)

---
+++ src/test/java/com/trl/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trl.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
