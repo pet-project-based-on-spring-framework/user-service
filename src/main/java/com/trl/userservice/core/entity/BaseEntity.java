579d52ee729a4c71a666de8e8cc6c4b5c1ed8eb3
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2021-01-29 20:36:07 +0100

    feat() adding BaseEntity (master)

---
+++ src/main/java/com/trl/userservice/core/entity/BaseEntity.java
@@ -0,0 +1,45 @@
+package com.trl.userservice.core.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+/**
+ * This class is designed to represent BasicEntity.
+ *
+ * @author Tsyupryk Roman
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    protected LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date", nullable = false)
+    protected LocalDateTime updatedDate;
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+}
+
