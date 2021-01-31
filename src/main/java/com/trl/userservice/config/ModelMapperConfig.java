da347f28a34a73d277f732ebbd8ea7bcf622542e
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2021-01-31 14:36:47 +0100

    feat(config) adding ModelMapperConfig (#1)

---
+++ src/main/java/com/trl/userservice/config/ModelMapperConfig.java
@@ -0,0 +1,21 @@
+package com.trl.userservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        final ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true);
+        return mapper;
+    }
+}
+
